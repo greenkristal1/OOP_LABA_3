23e2d6ce6a9ab5d7c9eb356e59221ce88428bacf
greenkristal1 <prototape.123456789@gmail.com>
2023-03-12 15:02:57 +0500

    Добавил класс Base, Circle, Car

--- LABA3_OOP/Program.cs
+++ LABA3_OOP/Program.cs
@@ -2,11 +2,68 @@
 
 namespace LABA3_OOP
 {
+
+    abstract class Base
+    {
+        public abstract string Name { get; init; }
+        public abstract void printName();
+
+        ~Base()
+        {
+
+        }
+    }
+
+
+    class Circle : Base
+    {
+
+        public override string Name { get; init; } = "Circle";
+        public int Radius { get; private set; }
+        public Circle()
+        {
+            Radius = 1;
+
+        }
+        public Circle(int length)
+        {
+            Radius = length;
+
+        }
+        public Circle(Circle c)
+        {
+            Console.WriteLine("Copy constructor");
+            this.Radius = c.Radius;
+        }
+        public override void printName()
+        {
+            Console.WriteLine($"The name of this object is {Name}");
+        }
+
+
+    }
+    class Car : Base
+    {
+        public override string Name { get; init; } = "Car";
+        public string Brand { get; private set; }
+        public Car()
+        {
+            Brand = "Toyota";
+        }
+
+        public Car(string brand)
+        {
+            Brand = brand;
+        }
+        public override void printName()
+        {
+            Console.WriteLine($"This is the object {Name}, brand is {Brand}");
+        }
+    }
+
+
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+
     }
 }

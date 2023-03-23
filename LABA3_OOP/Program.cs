f41709957b7874465126fc4517562121433ce9ab
greenkristal1 <prototape.123456789@gmail.com>
2023-03-23 21:01:34 +0500

    Добавил класс Storage, реализовал методы getCount и setObject

--- LABA3_OOP/Program.cs
+++ LABA3_OOP/Program.cs
@@ -60,9 +60,48 @@ namespace LABA3_OOP
             Console.WriteLine($"This is the object {Name}, brand is {Brand}");
         }
     }
+    class Storage
+    {
+        private Base[] arr;
+        private int size;
+
+        public Storage(int size)
+        {
+
+            arr = new Base[size];
+            this.size = size;
+        }
+        ~Storage()
+        {
+
+        }
+        public int getCount()
+        {
+            return size;
+        }
+        public void setObject(int index, Base o)
+        {
+            if (index < size && index >= 0 && arr[index] == null)
+                arr[index] = o;
+            else if (index >= 0)
+            {
+
+                Base[] temp = new Base[size + 1];
+                for (int i = 0; i < size; i++)
+                {
+                    temp[i] = arr[i];
+
+                }
+                temp[size] = o;
+                size++;
+                arr = temp;
+
+            }
+        }
+    }
 
 
-    class Program
+        class Program
     {
 
     }
